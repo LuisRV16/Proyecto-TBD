6ee68c817c1443355755c51cf66d2660aadeabcb
LuisR16 <turey0412@gmail.com>
2023-10-15 18:07:39 -0600

    Agregue el script de la base de datos para el proyecto e inclui actualizaciones del diagrama y el nuevo MR

---
+++ script BD proyecto.sql
@@ -0,0 +1,162 @@
+create schema concursoVEX;
+
+use concursoVEX;
+
+create table sedes(
+	id varchar(15) primary key,
+    nombre varchar(50) unique not null,
+    direccion varchar(80) unique not null
+);
+
+create table eventos(
+	id varchar(15) primary key,
+    nombre varchar(80) unique not null,
+    hora_i time,
+    hora_f time,
+    fecha date unique
+);
+
+create table juez(
+	id varchar(20) primary key,
+    nombre varchar(50) not null,
+    institucion varchar(50) not null,
+    nivel_academico varchar(20) not null
+);
+
+create table jurados(
+	nombre varchar(30) not null,
+    id_evento varchar(15) not null,
+    categoria enum ('primaria', 'secundaria', 'bachillerato', 'universidad') not null,
+    primary key(id_evento, nombre),
+    foreign key(id_evento) references eventos(id) on update cascade
+);
+
+create table juez_jurados(
+	id_juez varchar(20) not null,
+    id_evento varchar(15) not null,
+    nombre varchar(30) not null,
+    primary key (id_juez, id_evento, nombre),
+    foreign key (id_juez) references juez(id),
+    foreign key (id_evento, nombre) references jurados(id_evento, nombre) on update cascade
+);
+
+create table instituciones(
+	nombre varchar(100) not null,
+    id_evento varchar(15) not null,
+    direccion varchar(100) not null,
+    nivel_academico varchar(15) not null,
+    primary key(nombre, id_evento),
+    foreign key(id_evento) references eventos(id) on update cascade
+);
+
+create table asesores(
+	nombre varchar(50) unique not null,
+    nombre_institucion varchar(100) not null,
+    id_evento varchar(15) not null,
+    correo varchar(100) not null,
+    primary key(nombre, nombre_institucion, id_evento),
+    foreign key(nombre_institucion, id_evento)
+    references instituciones(nombre, id_evento) on update cascade
+);
+
+create table equipos(
+	nombre varchar(30) not null,
+    nombre_institucion varchar(100) not null,
+    id_evento varchar(15) not null,
+    nombre_asesor varchar(50) not null,
+    categoria enum ('primaria', 'secundaria', 'bachillerato', 'universidad') not null,
+    primary key(nombre, nombre_institucion, id_evento),
+    foreign key(nombre_institucion, id_evento) references instituciones(nombre, id_evento) ,
+    foreign key(nombre_asesor) references asesores(nombre) on update cascade
+);
+
+create table integrantes(
+	nombre varchar(50) not null,
+    nombre_equipo varchar(30) not null,
+    nombre_institucion varchar(100) not null,
+    id_evento varchar(15) not null,
+    fecha_nac date not null,
+   -- edad int not null,
+    primary key(nombre, nombre_equipo, nombre_institucion, id_evento),
+    foreign key(nombre_equipo, nombre_institucion, id_evento)
+    references equipos(nombre, nombre_institucion, id_evento) on delete cascade
+);
+
+create table diseno (
+    id varchar(3) primary key,
+    reg_fechas boolean not null,
+    just_de_cam_proto boolean not null,
+    diag_img boolean not null,
+    ortog_redacc boolean not null,
+    presentacion boolean not null,
+    dis_mod_autodesk boolean not null,
+    ana_elem boolean not null,
+    ensamble_proto boolean not null,
+    modelo_acorde_robot boolean not null,
+    acorde_robot boolean not null,
+    acorde_simulacion boolean not null,
+    restricc_mov boolean not null,
+    vid_anim boolean not null
+);
+
+create table programacion (
+    id varchar(3) primary key,
+    soft_robotc boolean not null,
+    uso_funciones boolean not null,
+    complejidad_prog boolean not null,
+    just_sec_prog boolean not null,
+    estrc_func_aplicadas boolean not null,
+    depuracion_cod boolean not null,
+    creacion_cod_mod boolean not null,
+    documentacion boolean not null,
+    vinculacion boolean not null,
+    declaracion_uso_dep boolean not null,
+    vinculo_joystick boolean not null,
+    eficiencia_calib boolean not null,
+    habilidad_joystick boolean not null,
+    respuesta_mando boolean not null,
+    docu_mod_driver boolean not null,
+    demo_15_seg boolean not null,
+    no_inconvenientes boolean not null,
+    demo_objetivo boolean not null,
+    exp_mod_auto boolean not null
+);
+
+create table construccion (
+    id varchar(3) primary key,
+    prototipo_estetico boolean not null,
+    estructuras_estables boolean not null,
+    sistemas boolean not null,
+    sensors boolean not null,
+    cableado boolean not null,
+    calculo_implementacion boolean not null,
+    alcance_robot boolean not null,
+    imp_estc_vex_rob boolean not null,
+    mono_core boolean not null,
+    analisis_estructuras boolean not null,
+    relacion_vel_ang boolean not null,
+    tren_engranes boolean not null,
+    centro_gravedad boolean not null,
+    sist_transmision boolean not null,
+    potencia boolean not null,
+    torque boolean not null,
+    velocidad boolean not null
+);
+
+
+create table evaluacion(
+	nombre_equipo varchar(30) not null,
+    nombre_institucion varchar(100) not null,
+    id_evento varchar(15) not null,
+    nombre_jurado varchar(30) not null,
+    id_diseno varchar(3) not null,
+    id_programacion varchar(3) not null,
+    id_construccion varchar(3) not null,
+    calificacion float,
+    primary key(nombre_equipo, nombre_institucion, id_evento, nombre_jurado, id_diseno, id_programacion, id_construccion),
+    foreign key(nombre_equipo, nombre_institucion, id_evento) references equipos(nombre, nombre_institucion, id_evento),
+    foreign key(id_evento, nombre_jurado) references jurados(id_evento, nombre),
+    foreign key(id_diseno) references diseno(id),
+    foreign key(id_programacion) references programacion(id),
+    foreign key(id_construccion) references construccion(id)
+);
